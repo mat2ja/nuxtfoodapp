2acacc767dd6a83e35cf01902a53a4cd696d4cda
Matija Osrecki <matija.osrecki@gmail.com>
2021-03-26 19:54:51 +0100

    configuring nuxt

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<main class="container">
+		<h1>Hi from home page!</h1>
+		<p>Hi from home page!</p>
+	</main>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			name: 'matija',
+		};
+	},
+};
+</script>
+
+<style>
+</style>
