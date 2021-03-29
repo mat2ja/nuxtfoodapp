050bc7b5693a46f92ebd78e79519552e3cbbbbfd
Matija Osrecki <matija.osrecki@gmail.com>
2021-03-29 16:29:09 +0200

    filtered the grid with resuable component

--- components/AppRestaurantInfo.vue
+++ components/AppRestaurantInfo.vue
@@ -1,6 +1,6 @@
 <template>
 	<section class="restaurantinfo">
-		<div v-for="store in fooddata" :key="store.id">
+		<div v-for="store in datasource" :key="store.id">
 			<h2>
 				{{ store.name }}
 			</h2>
@@ -31,11 +31,9 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
-
 export default {
-	computed: {
-		...mapState(['fooddata']),
+	props: {
+		datasource: [Array, Object],
 	},
 	methods: {
 		priceFormatting(item) {
--- pages/restaurants.vue
+++ pages/restaurants.vue
@@ -5,16 +5,17 @@
 
 			<AppSelect @change="selectedRestaurant = $event" />
 			<!-- awesome debugging trick! -->
-			<pre>{{ $data }}</pre>
+			<!-- <pre>{{ $data }}</pre> -->
 		</div>
 
-		<AppRestaurantInfo />
+		<AppRestaurantInfo :datasource="filteredRestaturants"/>
 	</main>
 </template>
 
 <script>
 import AppRestaurantInfo from '@/components/AppRestaurantInfo.vue';
 import AppSelect from '@/components/AppSelect.vue';
+import { mapState } from 'vuex';
 
 export default {
 	components: {
@@ -25,6 +26,18 @@ export default {
 		return {
 			selectedRestaurant: '',
 		};
+	},
+	computed: {
+		...mapState(['fooddata']),
+		filteredRestaturants() {
+			if (this.selectedRestaurant) {
+				return this.fooddata.filter((el) => {
+					let name = el.name.toLowerCase();
+					return name.includes(this.selectedRestaurant);
+				});
+			}
+			return this.fooddata;
+		},
 	},
 };
 </script>
