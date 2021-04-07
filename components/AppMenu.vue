41930f2a86767a7242c9bc1398ee061f901b2858
Matija Osrecki <matija.osrecki@gmail.com>
2021-04-07 14:50:41 +0200

    finished the cart

--- components/AppMenu.vue
+++ components/AppMenu.vue
@@ -8,16 +8,21 @@
 			</li>
 			<li><NuxtLink to="/restaurants">Restaurants</NuxtLink></li>
 		</ul>
+		<div class="smallnum" v-if="cartCount > 0">{{ cartCount }}</div>
 		<NuxtLink to="/cart">Cart</NuxtLink>
 	</nav>
 </template>
 
 <script>
 import AppLogo from '@/components/AppLogo.vue';
+import { mapGetters } from 'vuex';
 
 export default {
 	components: {
 		AppLogo,
+	},
+	computed: {
+		...mapGetters(['cartCount']),
 	},
 };
 </script>
--- pages/cart.vue
+++ pages/cart.vue
@@ -1,13 +1,57 @@
 <template>
 	<main class="container cart">
-		<h2>Cart</h2>
-		<div v-for="item in $store.state.cart" :key="item.id">
-			<pre>{{ item }}</pre>
-		</div>
+		<h2>Your Cart</h2>
+
+		<table v-if="cart.length">
+			<thead>
+				<th>Item</th>
+				<th>AddOns</th>
+				<th>Amount</th>
+				<th>Total Price</th>
+			</thead>
+
+			<tbody>
+				<tr v-for="item in cart" :key="item.id">
+					<td>
+						{{ item.item }}
+						<span v-if="item.options.length > 0"
+							>- {{ item.options }}</span
+						>
+					</td>
+					<td>
+						<span
+							class="comma"
+							v-for="addOn in item.addOns"
+							:key="addOn"
+							>{{ addOn }}</span
+						>
+					</td>
+					<td>{{ item.count }}</td>
+					<td>${{ item.combinedPrice }}</td>
+				</tr>
+				<tr>
+					<td colspan="3"></td>
+					<td class="total">Total: ${{ totalPrice }}</td>
+				</tr>
+			</tbody>
+		</table>
+		<AppCartEmpty v-else />
 	</main>
 </template>
 
 <script>
-export default {};
-</script>
+import { mapState } from 'vuex';
+import { mapGetters } from 'vuex';
+import AppCartEmpty from '@/components/AppCartEmpty.vue';
 
+export default {
+	components: {
+		AppCartEmpty,
+	},
+	computed: {
+		...mapState(['cart']),
+		...mapGetters(['totalPrice']),
+	},
+};
+</script>
+ 
